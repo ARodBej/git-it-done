da4e6090acb9a63b7fcc4890a297a3433c765c6a
Adilene Bejines <adi3511@hotmail.com>
2021-11-29 18:12:51 -0700

    updated request logic with dynamic variables

--- assets/js/homepage.js
+++ assets/js/homepage.js
@@ -1,5 +1,11 @@
 var getUserRepos = function() {
-    fetch("https://api.github/users.octocat/repos");
-};
+    var apiUrl = "https://api.github.com/users/" + user + "/repos";
 
+    //make a request to the url
+    fetch(apiUrl).then(function(response) {
+        response.json().then(function(data) {
+            console.log(data);
+        });
+    });
+};
 getUserRepos();
